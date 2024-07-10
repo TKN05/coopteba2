40d65506cf1bc5fd47ba866542b73fcb65886586
Martin <165105676+TKN05@users.noreply.github.com>
2024-07-10 18:06:26 +0000

    Borre header y page ejemplo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Proyectos from './pages/Proyectos';
-import EjemploProyecto from './pages/Proyectos';
-import Header from './components/Header';
+// import EjemploProyecto from './pages/Proyectos';
+// import Header from './components/Header';
 import './App.css'; // Asegúrate de tener tu archivo CSS para estilos
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/proyectos" element={<Proyectos />} />
-            <Route path="/ejemplo-proyecto" element={<EjemploProyecto />} />
+           
           </Routes>
         </main>
       </div>
