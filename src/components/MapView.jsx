aee293c5bbed599f5c883fb02c6c787a7b4fa181
RodrigoRoa06 <115632828+RodrigoRoa06@users.noreply.github.com>
2024-07-29 15:32:54 -0300

    .

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -12,7 +12,7 @@ const customIcon = L.icon({
     popupAnchor: [0, -40] // Punto desde donde se abre el popup del marcador [arriba, centrado]
     });
     
-function MapView ({lat=-33.65099943765083, lng=-58.62191964990408}){
+function MapView ({lat=-25.65099944465083, lng=-68.62191923990408}){
 
 
     return (
